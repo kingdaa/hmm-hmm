0a21dcb6d9df679719a670601b2a24d466ecfed8
kingdaa <kinghuang90@gmail.com>
2015-06-19 22:26:55 -0700

    Genereate Unique Integer

--- src/GenerateUniqueInteger.java
+++ src/GenerateUniqueInteger.java
@@ -1,5 +1,16 @@
-/**
- * Created by King on 6/2/15.
+import java.util.concurrent.atomic.AtomicInteger;
+
+/*
+  Generate Unique Integer 
+  1. Each time generates an unique integer 
+  2. Should be thread-safe: different threads never get same number
  */
+
 public class GenerateUniqueInteger {
+	private AtomicInteger ai = new AtomicInteger();
+
+	// Thought 1: Use an AtomicInteger, increment and get each time
+	public Integer getUniqueInt() {
+		return ai.incrementAndGet();
+	}
 }
